910936ecffa0a0ced09aeac5932cb6f10ed160dd
Nishant Bhardwaj <52878003+Nishant-Bhardwaj@users.noreply.github.com>
2023-06-29 10:08:01 +0530

    File Based Rolling Logging functionality

--- loan-orchestration/src/main/java/org/bfsi/orchestration/OrchestrationServiceApplication.java
+++ loan-orchestration/src/main/java/org/bfsi/orchestration/OrchestrationServiceApplication.java
@@ -1,12 +1,23 @@
 package org.bfsi.orchestration;
 
+import jakarta.annotation.PostConstruct;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class OrchestrationServiceApplication {
+
+    Logger logger = LogManager.getLogger(OrchestrationServiceApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(OrchestrationServiceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void logger(){
+        logger.info("INFO--->");
+        logger.error("ERROR--->");
     }
 }
